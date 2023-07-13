6c47127d027996f9701a10c8a82fbd804e795e21
LeonardoCD <leonardofelipe931@gmail.com>
2023-07-13 10:02:14 -0400

    feat: add initial search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import TripSearch from "./components/TripSearch";
 
 export default function Home() {
-  const { data } = useSession();
-
   return (
     <div>
-      <div className="flex flex-col">
-        <button onClick={() => signIn()}>Login</button>
-        <button onClick={() => signOut()}>Logout</button>
-      </div>
-      <h1>Olá, {data?.user?.name}</h1>
-      <h1>Olá, {data?.user?.email}</h1>
-      <h1>Olá, {data?.user?.image}</h1>
-      <img src={data?.user?.image ?? ""} alt="" />
+      <TripSearch />
     </div>
   );
 }
