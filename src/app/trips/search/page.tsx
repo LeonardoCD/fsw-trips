d30ed23994941a02c02952b4ac12f56b68e1780a
LeonardoCD <leonardofelipe931@gmail.com>
2023-07-25 11:39:44 -0400

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,18 +29,18 @@ export default function Trips() {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col p-5 items-center">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col p-5 items-center lg:items-start">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
         Viagens Encontradas
       </h1>
 
-      <h2 className="text-grayPrimary font-medium mb-5">
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
         {trips.length > 0
           ? "Listamos as melhores viagens para você!"
           : "Não encontramos nada nos seus parâmetros! =("}
       </h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
         {trips.map((trip) => (
           <TripItem trip={trip} key={trip.id} />
         ))}
