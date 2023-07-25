309977f99c247f09d9812f2b45d983bf7d2e604a
LeonardoCD <leonardofelipe931@gmail.com>
2023-07-25 15:12:36 -0400

    style: adapt trip details to desktop

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -7,12 +7,12 @@ interface TripHighlightsProps {
 
 export default function TripHighlights({ highlights }: TripHighlightsProps) {
   return (
-    <div className="flex flex-col p-5">
-      <h2 className="font-semibold text-primaryDarker mb-2">Destaques</h2>
+    <div className="flex flex-col p-5 lg:p-0 lg:mt-12">
+      <h2 className="font-semibold text-primaryDarker mb-2 lg:text-xl">Destaques</h2>
 
-      <div className="flex flex-wrap gap-y-3">
+      <div className="flex flex-wrap gap-y-3 lg:mt-5">
         {highlights.map((highlight) => (
-          <div key={highlight} className="flex items-center gap-2 w-1/2">
+          <div key={highlight} className="flex items-center gap-2 lg:gap-3 w-1/2">
             <Image
               src="/check-icon.png"
               width={15}
@@ -20,7 +20,7 @@ export default function TripHighlights({ highlights }: TripHighlightsProps) {
               alt={highlight}
             />
 
-            <p className="text-xs leading-5 text-grayPrimary">{highlight}</p>
+            <p className="text-xs leading-5 text-grayPrimary lg:text-base">{highlight}</p>
           </div>
         ))}
       </div>
--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -12,10 +12,10 @@ export default function TripLocation({
   locationDescription,
 }: TripLocationProps) {
   return (
-    <div className="p-5">
-      <h2 className="font-semibold text-primaryDarker mb-5">Localização</h2>
+    <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
+      <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Localização</h2>
 
-      <div className="relative h-[280px] w-full">
+      <div className="relative h-[280px] w-full lg:hidden">
         <Image
           src="/map-mobile.png"
           alt={location}
@@ -27,11 +27,23 @@ export default function TripLocation({
         />
       </div>
 
-      <h3 className="text-primaryDarker text-sm font-semibold mt-3">
+      <div className="hidden relative h-[480px] w-full lg:block">
+        <Image
+          src="/map-desktop.png"
+          alt={location}
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          className="rounded-lg shadow-md"
+        />
+      </div>
+
+      <h3 className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">
         {location}
       </h3>
 
-      <p className="text-xs text-primaryDarker mt-2 leading-5">
+      <p className="text-xs text-primaryDarker mt-2 leading-5 lg:text-sm lg:mt-4">
         {locationDescription}
       </p>
 
